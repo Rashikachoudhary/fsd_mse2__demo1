832eec83e4e931d640d4883883aac7f82ebea7e8
Rashikachoudhary <rashikachoudhary123@gmail.com>
2026-04-21 21:56:53 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -6,7 +6,7 @@ function Login() {
 
   const login = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/api/login", form);
+      const res = await axios.post("https://your-backend.onrender.com/api/login", form);
 
       localStorage.setItem("token", res.data.token);
 
